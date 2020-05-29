623c6770bbff71c96b7c277f29f6dbbe9ee8326c
franck-gaspoz <franck.gaspoz@gmail.com>
2020-05-29 04:32:26 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static DotNetConsoleSdk.Component.CLI.CLI;
+using static DotNetConsoleSdk.DotNetConsole;
+
+namespace DotNetConsoleSdkSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeCLI(args);
+            if (HasArgs)
+                Print(Arg(0));
+            else
+            {
+                //RunSampleCLI("(f=yellow,exec=[[System.IO.Path.GetFileName(System.Environment.CurrentDirectory)]]) > ");
+                var returnCode = ShellSample.RunShell(args, "(f=yellow)> ");
+                Environment.Exit(returnCode);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# dotnet-console-sdk-sample
+dotnet console sdk helps build fastly nice multi-plateforms (windows,linux,macos) console applications with C#. These sample console app shows how to use it accross several examples
+<hr/>
+
+
+<u>packages dependencies:</u>
+
+Microsoft.CodeAnalysis.CSharp.Scripting 3.7.0-1.final
